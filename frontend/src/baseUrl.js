76c2beda878cc11aca78ae5551346ba6bfeb4a60
Himilsinh Sindha <102691151+himilsinh-sindha@users.noreply.github.com>
2023-10-10 23:36:51 +0530

    Delete frontend directory

--- frontend/src/baseUrl.js
+++
@@ -1,3 +0,0 @@
-export const baseApiURL = () => {
-  return "https://csproconnect.onrender.com";
-};
--- frontend/src/redux/action.js
+++
@@ -1,2 +0,0 @@
-export const USER_LOGIN_ID = "USER_LOGIN_ID";
-export const USER_DATA = "USER_DATA";
