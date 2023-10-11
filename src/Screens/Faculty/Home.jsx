07c4ad23bbbb340918893cfa0fbc9d4b64a86da3
Manav Parmar <manavparmar0903@gmail.com>
2023-10-11 21:57:40 +0530

    Added Attendance Module

--- src/Screens/Faculty/Home.jsx
+++ src/Screens/Faculty/Home.jsx
@@ -8,6 +8,7 @@ import { Toaster } from "react-hot-toast";
 import Material from "./Material";
 import Marks from "./Marks";
 import Student from "./Student";
+import Attendance from "./Attendance";
 const Home = () => {
   const router = useLocation();
   const navigate = useNavigate();
@@ -86,9 +87,20 @@ const Home = () => {
             >
               Material
             </li>
+            <li
+              className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                selectedMenu === "Attendance"
+                  ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                  : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+              }`}
+              onClick={() => setSelectedMenu("Attendance")}
+            >
+              Attendance
+            </li>
           </ul>
           <>
             {selectedMenu === "Timetable" && <Timetable />}
+            {selectedMenu === "Attendance" && <Attendance />}
             {selectedMenu === "Upload Marks" && <Marks />}
             {selectedMenu === "Material" && <Material />}
             {selectedMenu === "Notice" && <Notice />}
