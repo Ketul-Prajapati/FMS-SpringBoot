f4be8e920ab4857e38e6054c80150e6b8ceafda0
Manav Parmar <manavparmar0903@gmail.com>
2023-10-11 23:13:14 +0530

    Update Attendance.jsx

--- src/Screens/Faculty/Attendance.jsx
+++ src/Screens/Faculty/Attendance.jsx
@@ -1,45 +1,5 @@
-import axios from "axios";
-import React, { useEffect, useState, useCallback } from "react";
-import Heading from "../../components/Heading";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import toast from "react-hot-toast";
-import { storage } from "../../firebase/config";
-import { baseApiURL } from "../../baseUrl";
-const Attendance = () => {
-    const [addselected, setAddSelected] = useState({
-        branch: "",
-        class: "",
-        link: "",
-    });
-    // const addAttendanceHandler = useCallback(() => {
-    //     toast.loading("Adding Attendance");
-    //     const headers = {
-    //         "Content-Type": "application/json",
-    //     };
-    //     axios
-    //         .post(`${baseApiURL()}/Attendance/addAttendance`, addselected, {
-    //             headers: headers,
-    //         })
-    //         .then((response) => {
-    //             toast.dismiss();
-    //             if (response.data.success) {
-    //                 toast.success(response.data.message);
-    //                 setAddSelected({
-    //                     branch: "",
-    //                     class: "",
-    //                     link: "",
-    //                 });
-    //             } else {
-    //                 console.log(response);
-    //                 toast.error(response.data.message);
-    //             }
-    //         })
-    //         .catch((error) => {
-    //             toast.dismiss();
-    //             toast.error(error.response.data.message);
-    //         });
-    // }, [addselected]);
 
+import React from "react";
 
     return (
         <div className="w-[85%] mx-auto mt-10 flex justify-center items-start flex-col mb-10">
@@ -51,12 +11,8 @@ const Attendance = () => {
                     <p className="mb-4 text-xl font-medium">Select Class for Attendance</p>
 
                     <select
-                        onChange={(e) =>
-                            setAddSelected({ ...addselected, class: e.target.value })
-                        }
-                        value={addselected.class}
-                        name="branch"
-                        id="branch"
+                        name="class name"
+                        id="class"
                         className="px-2 bg-blue-50 py-3 rounded-sm text-base w-[80%] accent-blue-700 mt-4"
                     >
                         <option defaultValue>-- Select Class --</option>
@@ -70,6 +26,5 @@ const Attendance = () => {
             </div>
         </div>
     );
-};
 
 export default Attendance;
