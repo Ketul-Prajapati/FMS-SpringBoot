8e7a08541c248db8f001a2b8b39284ec2ee42481
Manav Parmar <manavparmar0903@gmail.com>
2023-10-11 22:28:08 +0530

    Update Attendance.jsx

--- src/Screens/Faculty/Attendance.jsx
+++ src/Screens/Faculty/Attendance.jsx
@@ -1,103 +1,130 @@
-import React, { useState } from "react";
-import toast from "react-hot-toast";
-import Heading from "../../components/Heading";
 import axios from "axios";
+import React, { useEffect, useState ,useCallback} from "react";
+import { FiUpload } from "react-icons/fi";
+import Heading from "../../components/Heading";
+import { AiOutlineClose } from "react-icons/ai";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import toast from "react-hot-toast";
+import { storage } from "../../firebase/config";
 import { baseApiURL } from "../../baseUrl";
-import { FiSearch } from "react-icons/fi";
 const Attendance = () => {
-    const [search, setSearch] = useState();
-    const [data, setData] = useState({
-        enrollmentNo: "",
-        firstName: "",
-        middleName: "",
-        lastName: "",
-        email: "",
-        phoneNumber: "",
-        semester: "",
-        branch: "",
-        gender: "",
-        profile: "",
-    });
-    const [id, setId] = useState();
+  const [addselected, setAddSelected] = useState({
+    branch: "",
+    semester: "",
+    link: "",
+  });
+  const [file, setFile] = useState();
+  const [branch, setBranch] = useState();
 
-    const searchStudentHandler = (e) => {
-        setId("");
-        setData({
-            enrollmentNo: "",
-            firstName: "",
-            middleName: "",
-            lastName: "",
-            email: "",
-            phoneNumber: "",
-            semester: "",
-            branch: "",
-            gender: "",
-            profile: "",
-        });
-        e.preventDefault();
-        toast.loading("Getting Student");
-        const headers = {
-            "Content-Type": "application/json",
-        };
-        axios
-            .post(
-                `${baseApiURL()}/student/details/getDetails`,
-                { enrollmentNo: search },
-                { headers }
-            )
-            .then((response) => {
-                toast.dismiss();
-                if (response.data.success) {
-                    if (response.data.user.length === 0) {
-                        toast.error("No Student Found!");
-                    } else {
-                        toast.success(response.data.message);
-                        setData({
-                            enrollmentNo: response.data.user[0].enrollmentNo,
-                            firstName: response.data.user[0].firstName,
-                            middleName: response.data.user[0].middleName,
-                            lastName: response.data.user[0].lastName,
-                            email: response.data.user[0].email,
-                            phoneNumber: response.data.user[0].phoneNumber,
-                            semester: response.data.user[0].semester,
-                            branch: response.data.user[0].branch,
-                            gender: response.data.user[0].gender,
-                            profile: response.data.user[0].profile,
-                        });
-                        setId(response.data.user[0]._id);
-                    }
-                } else {
-                    toast.error(response.data.message);
-                }
-            })
-            .catch((error) => {
-                toast.error(error.response.data.message);
-                console.error(error);
-            });
+  useEffect(() => {
+    getBranchData();
+  }, []);
+  const addTimetableHandler = useCallback(() => {
+    toast.loading("Adding Timetable");
+    const headers = {
+      "Content-Type": "application/json",
     };
+    axios
+      .post(`${baseApiURL()}/timetable/addTimetable`, addselected, {
+        headers: headers,
+      })
+      .then((response) => {
+        toast.dismiss();
+        if (response.data.success) {
+          toast.success(response.data.message);
+          setAddSelected({
+            branch: "",
+            semester: "",
+            link: "",
+          });
+          setFile("");
+        } else {
+          console.log(response);
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.dismiss();
+        toast.error(error.response.data.message);
+      });
+  }, [addselected]);
 
-    return (
-        <div className="w-[85%] mx-auto mt-10 flex justify-center items-start flex-col mb-10">
-            <div className="flex justify-between items-center w-full">
-                <Heading title="Attendance Details" />
-            </div>
-            <select
-                onChange={(e) =>
-                    setAddSelected({ ...addselected, semester: e.target.value })
-                }
-                value={addselected.semester}
-                name="branch"
-                id="branch"
-                className="px-2 bg-blue-50 py-3 rounded-sm text-base w-[80%] accent-blue-700 mt-4"
-            >
-                <option defaultValue>-- Select Class --</option>
-                <option value="1">BE-I</option>
-                <option value="2">BE-II</option>
-                <option value="3">BE-III</option>
-                <option value="4">BE-IV</option>
-            </select>
+  useEffect(() => {
+    const uploadFileToStorage = async (file) => {
+      toast.loading("Upload Timetable To Server");
+      const storageRef = ref(
+        storage,
+        `Timetable/${addselected.branch}/Semester ${addselected.semester}`
+      );
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {
+          console.error(error);
+          toast.dismiss();
+          // toast.error("Something Went Wrong!");
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            toast.dismiss();
+            setFile();
+            toast.success("Timetable Uploaded To Server");
+            setAddSelected({ ...addselected, link: downloadURL });
+            addTimetableHandler();
+          });
+        }
+      );
+    };
+    file && uploadFileToStorage(file);
+  }, [file,addTimetableHandler,addselected]);
+
+  const getBranchData = () => {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .get(`${baseApiURL()}/branch/getBranch`, { headers })
+      .then((response) => {
+        if (response.data.success) {
+          setBranch(response.data.branches);
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error.message);
+      });
+  };
+
+  return (
+    <div className="w-[85%] mx-auto mt-10 flex justify-center items-start flex-col mb-10">
+      <div className="flex justify-between items-center w-full">
+        <Heading title={`Upload Timetable`} />
+      </div>
+      <div className="w-full flex justify-evenly items-center mt-12">
+        <div className="w-1/2 flex flex-col justify-center items-center">
+          <p className="mb-4 text-xl font-medium">Add Attendance</p>
+          <select
+            onChange={(e) =>
+              setAddSelected({ ...addselected, semester: e.target.value })
+            }
+            value={addselected.semester}
+            name="branch"
+            id="branch"
+            className="px-2 bg-blue-50 py-3 rounded-sm text-base w-[80%] accent-blue-700 mt-4"
+          >
+            <option defaultValue>-- Select Class --</option>
+            <option value="1">BE-1</option>
+            <option value="2">BE-2</option>
+            <option value="3">BE-3</option>
+            <option value="4">BE-4</option>
+          </select>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Attendance;
