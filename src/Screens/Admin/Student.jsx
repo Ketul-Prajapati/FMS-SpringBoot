ecbabc69904f0a75522a2d9c3e8f5476319c2698
Himilsinh-Sindha <himilsinh07@gmail.com>
2023-10-12 20:09:17 +0530

    Update Student.jsx

--- src/Screens/Admin/Student.jsx
+++ src/Screens/Admin/Student.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 import Heading from "../../components/Heading";
 import axios from "axios";
