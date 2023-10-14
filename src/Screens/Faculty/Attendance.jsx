f6d7b3828442e5f8b0bd0bd9903d425aad6d5f3d
Himilsinh-Sindha <himilsinh07@gmail.com>
2023-10-14 17:30:16 +0530

    completed Attendance module

--- src/Screens/Faculty/Attendance.jsx
+++ src/Screens/Faculty/Attendance.jsx
@@ -1,12 +1,51 @@
 import React, { useState } from "react";
 import "jspdf-autotable";
+import { SlNote } from "react-icons/sl";
+import { FaFilePdf ,FaFileExcel } from "react-icons/fa";
+import toast from "react-hot-toast";
+import Heading from "../../components/Heading";
+import axios from "axios";
+import { baseApiURL } from "../../baseUrl";
+import dayjs from "dayjs";
+import * as XLSX from "xlsx";
 
 function Attendance() {
-  const [students, setStudents] = useState([
-    { rollNo: 1, name: "Student 1", isPresent: false },
-    { rollNo: 2, name: "Student 2", isPresent: false },
-    // Add more students as needed
-  ]);
+  const [search, setSearch] = useState("");
+  const [students, setStudents] = useState([]);
+  const [showTable, setShowTable] = useState(false);
+
+  // Updated searchStudentHandler to fix setting state
+  const searchStudentHandler = (e) => {
+    e.preventDefault();
+    toast.loading("Getting students list");
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post(
+        `${baseApiURL()}/student/details/getDetails`,
+        { class: search },
+        { headers }
+      )
+      .then((response) => {
+        toast.dismiss();
+        if (response.data.success) {
+          if (response.data.user.length === 0) {
+            toast.error("No Students Found!");
+          } else {
+            toast.success(response.data.message);
+            setStudents(response.data.user);
+            setShowTable(true);
+          }
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+        console.error(error);
+      });
+  };
 
   const handleAttendanceChange = (index) => {
     const updatedStudents = [...students];
@@ -14,76 +53,148 @@ function Attendance() {
     setStudents(updatedStudents);
   };
 
-  const handleSubmit = () => {
-    // Assuming you want to send the attendance data to the server
-    fetch("YOUR_SERVER_ENDPOINT", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(students),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Attendance submitted successfully:", data);
-      })
-      .catch((error) => {
-        console.error("Error submitting attendance:", error);
-      });
+  const exportFileName = () => {
+    const currentDateTime = dayjs().format("DD-MM-YYYY HH:mm");
+    return `${search}_Attendance_${currentDateTime}`;
   };
+
   const handleDownloadExcel = () => {
-    // Importing 'xlsx' library
-    import("xlsx").then((XLSX) => {
-      const ws = XLSX.utils.json_to_sheet(students);
-      const wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, ws, "Attendance Sheet");
-      XLSX.writeFile(wb, "attendance.xlsx");
-    });
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.aoa_to_sheet([
+      ["PRN", "Name", "Present/Absent"],
+      ...students.map((student) => [
+        student.enrollmentNo,
+        `${student.lastName} ${student.firstName} ${student.middleName}`,
+        student.isPresent ? "Present" : "Absent",
+      ]),
+    ]);
+
+    XLSX.utils.book_append_sheet(wb, ws, "Attendance Sheet");
+    XLSX.writeFile(wb, `${exportFileName()}.xlsx`);
   };
 
   const handleDownloadPDF = () => {
     // Importing 'jspdf' library
     import("jspdf").then((jsPDF) => {
       const doc = new jsPDF.default();
+      doc.setFont("helvetica"); // Set font type
+      doc.setFontSize(12); // Set font size
+      const pdfTitle = `Attendance Of Class ${search} - ${dayjs().format("DD-MM-YYYY HH:mm")}`;
+      doc.text(pdfTitle, 60, 10);
+      const columns = ["PRN", "NAME", "PRESENT/ABSENT"];
+      const rows = students.map((student) => [
+        student.enrollmentNo,
+        `${student.lastName} ${student.firstName} ${student.middleName}`,
+        student.isPresent ? "Present" : "Absent",
+      ]);
       doc.autoTable({
-        head: [["Roll No.", "Name", "Present"]],
-        body: students.map((student) => [student.rollNo, student.name, student.isPresent ? "Present" : "Absent"]),
+        head: [columns],
+        body: rows,
       });
-      doc.save("attendance.pdf");
+      doc.save(`${exportFileName()}.pdf`);
     });
   };
 
   return (
     <div className="w-[85%] mx-auto mt-10 flex justify-center items-start flex-col mb-10">
-      {/* ...Existing JSX code... */}
-      {/* Add your table here */}
-      <table>
-        {/* Add your table headers */}
-        <thead>
-          <tr>
-            <th>Roll No.</th>
-            <th>Name</th>
-            <th>Present/Absent</th>
-          </tr>
-        </thead>
-        {/* Add your table body */}
-        <tbody>
-          {students.map((student, index) => (
-            <tr key={index}>
-              <td>{student.rollNo}</td>
-              <td>{student.name}</td>
-              {/* Add a checkbox or dropdown for marking attendance */}
-              <td><input type='checkbox' checked={student.isPresent} onChange={() => handleAttendanceChange(index)} /></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {/* Add your buttons here */}
-      <button onClick={handleSubmit}>Submit Attendance</button>
-      <button onClick={handleDownloadExcel}>Download Excel</button>
-      <button onClick={handleDownloadPDF}>Download PDF</button>
+      <div className="flex justify-between items-center w-full">
+        <Heading title={`Attendance`} />
+      </div>
+      <div className="w-full flex justify-center items-center mt-12">
+        <div className="w-full flex flex-col justify-center items-center">
+          <p className="mb-4 text-xl font-medium">Select Class For Attendance</p>
+          <div className="w-full flex justify-center items-center">
+            <select
+              name="class name"
+              id="class"
+              className="px-2 ml-40 bg-blue-50 py-3 rounded-sm text-base w-[40%] accent-blue-700 mt-4"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            >
+              <option defaultValue>-- Select Class --</option>
+              <option value="BE-I">BE-I</option>
+              <option value="BE-II">BE-II</option>
+              <option value="BE-III">BE-III</option>
+              <option value="BE-IV">BE-IV</option>
+              <option value="MCA-I">MCA-I</option>
+              <option value="MCA-II">MCA-II</option>
+            </select>
+            <button
+              className="relative ml-3 mt-3 flex justify-center items-center border-2 border-red-500 px-3 py-2 rounded text-red-500"
+              onClick={searchStudentHandler}
+            >
+              Take Attendance
+              <span className="ml-2">
+                <SlNote className="text-red-500 text-xl" />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 w-full">
+        {showTable && students.length > 0 && (
+          <div className="border border-blue-200 shadow-lg rounded-lg mb-4">
+            <table className="w-full border-collapse">
+              <thead>
+                <tr className="bg-blue-300">
+                  <th className="py-2 px-4 border border-blue-700">PRN</th>
+                  <th className="py-2 px-4 border border-blue-700">Name</th>
+                  <th className="py-2 px-4 border border-blue-700">Present/Absent</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                {students.sort((a, b) => {
+                  const lastNameCompare = a.lastName.localeCompare(b.lastName);
+                  if (lastNameCompare !== 0) {
+                    return lastNameCompare;
+                  }
+                  return a.firstName.localeCompare(b.firstName);
+                }).map((item, index) => (
+                  <tr key={index} className={index % 2 === 0 ? 'bg-white' : 'bg-blue-50'}>
+                    <td className="py-2 px-4 border border-blue-700 text-center">{item.enrollmentNo}</td>
+                    <td className="py-2 px-4 border border-blue-700 text-center">{`${item.lastName} ${item.firstName} ${item.middleName}`}</td>
+                    <td className="py-2 px-4 border border-blue-700 text-center">
+                      <input id="bordered-checkbox-1"
+                        type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-60"
+                        checked={item.isPresent}
+                        onChange={() => handleAttendanceChange(index)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>)}
+        <div className="flex justify-center space-x-4 mt-12">
+          {showTable && students.length > 0 && (
+            <>
+              <button
+                className="px-4 py-2 mr-8 text-xl flex justify-center items-center text-white bg-blue-500 hover:bg-blue-600 rounded-md"
+                onClick={handleDownloadExcel}
+              >
+                Download Excel
+                <span className="ml-2">
+                <FaFileExcel/>
+              </span>
+              </button>
+
+              <button
+                className="px-4 py-2 ml-8 text-xl flex justify-center items-center text-white bg-blue-500 hover:bg-blue-600 rounded-md"
+                onClick={handleDownloadPDF}
+              >
+                Download PDF
+                <span className="ml-2">
+                <FaFilePdf/>
+              </span>
+              </button>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
+
 }
 
 export default Attendance;
